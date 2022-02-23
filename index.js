Looky1173/Itinerary-Server

commit 86a3eb929193a7074331a0c29365212fbe857bcb

Fix for #1

index.js
@@ -439,6 +439,10 @@ app.put('/api/jams/:jam?', cors(corsOptions), async (req, res) => {
             // Update the jam and return the new slug
             await Jams.findOneAndUpdate({ slug: req.params.jam }, { $set: updateQuery }, { new: true }).then((updatedDocument) => {
                 if (updatedDocument) {
+                    if (updatedDocument.slug != req.params.jam) {
+                        // "Re-link" managers to the game jam if its slug was modified
+                        Managers.updateMany({ jam: req.params.jam }, { jam: updatedDocument.slug });
+                    }
                     return res.status(200).json({ ok: { newSlug: updatedDocument.slug } });
                 } else {
                     return res.status(404).json({ error: { status: 404, code: 'jamNotFound', detail: 'The requested jam could not be found.' } });

commit 63c22a2ad71d3f8901e867d4f18c00850303b664

Fix regression introduced in 86a3eb929193a7074331a0c29365212fbe857bcb

index.js
@@ -441,7 +441,7 @@ app.put('/api/jams/:jam?', cors(corsOptions), async (req, res) => {
                 if (updatedDocument) {
                     if (updatedDocument.slug != req.params.jam) {
                         // "Re-link" managers to the game jam if its slug was modified
-                        Managers.updateMany({ jam: req.params.jam }, { jam: updatedDocument.slug });
+                        Managers.updateMany({ jam: req.params.jam }, { $set: { jam: updatedDocument.slug } });
                     }
                     return res.status(200).json({ ok: { newSlug: updatedDocument.slug } });
                 } else {